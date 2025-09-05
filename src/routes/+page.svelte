9065b30e64d2e82111929da44705ebe37602ff7f
Daffa Ilhami <mdaffailhami@gmail.com>
2025-09-05 23:22:32 +0800

    Add Banner, Tentang, Visi, & Misi.. (Tapi baru Banner yg responsif)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,84 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  import banner from '$lib/assets/banner.png';
+  import logo2 from '$lib/assets/logo2.png';
+  import btc from '$lib/assets/crypto-icons/btc.png';
+  import eth from '$lib/assets/crypto-icons/eth.png';
+  import sol from '$lib/assets/crypto-icons/sol.png';
+  import sui from '$lib/assets/crypto-icons/sui.png';
+</script>
+
+<main class="flex flex-col items-center gap-y-8">
+  <section class="relative">
+    <div
+      class="h-[36rem] w-screen bg-cover bg-center brightness-30"
+      style="background-image: url({banner});"
+    ></div>
+    <div class="absolute top-0 left-0 flex h-full w-full items-center justify-center">
+      <section class="flex flex-col items-center">
+        <img src={logo2} alt="Logo" class="w-48 sm:w-56 lg:w-64" />
+        <h1 class="mt-6 text-center text-3xl font-bold text-white sm:text-4xl lg:text-5xl">
+          CryptoSharia
+        </h1>
+        <p class="mt-2 text-center text-base text-white sm:text-xl lg:text-2xl">
+          Platform Edukasi dan Informasi Aset Kripto Syariah
+        </p>
+      </section>
+    </div>
+  </section>
+
+  <section
+    class="flex max-w-[100rem] flex-row items-stretch justify-between px-4 sm:px-6 md:px-12 lg:px-24 xl:px-36"
+  >
+    <div class="flex flex-1 flex-col rounded-4xl border border-slate-200 p-8 shadow-md">
+      <h1 class="mb-1.5 text-4xl font-bold text-orange-600">Tentang <u><i>CryptoSharia</i></u></h1>
+      <p class="text-justify text-lg">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse dictum lacinia
+        elementum. Praesent molestie vel sem rutrum accumsan. Curabitur fermentum nec erat eu
+        iaculis. Nullam urna mauris, varius in luctus et, faucibus id elit. Nam commodo elit nec
+        mauris rhoncus pellentesque.<br /><br />Fusce id pretium ligula. Proin tincidunt facilisis
+        purus, non commodo metus. Quisque nec tellus et turpis facilisis rutrum. Praesent in egestas
+        odio. Donec at faucibus mauris. Mauris justo magna, suscipit ut euismod eu, pharetra vitae
+        diam.
+      </p>
+      <div class="flex-1"></div>
+      <div class="flex flex-row items-center justify-around gap-6 pt-6">
+        <img src={btc} alt="Bitcoin" class="size-10" />
+        <img src={eth} alt="Ethereum" class="size-10" />
+        <img src={sol} alt="Solana" class="size-10" />
+        <img src={sui} alt="Sui" class="size-10" />
+      </div>
+    </div>
+    <div class=" mx-10 w-[1px] bg-orange-300"></div>
+    <div class="flex flex-1 flex-col p-8 pl-0">
+      <div>
+        <div class="relative">
+          <div
+            class="absolute top-1/2 -left-12 size-4 -translate-y-1/2 rounded-full bg-orange-600"
+          ></div>
+          <h1 class="mb-1.5 text-4xl font-bold text-orange-600">Visi</h1>
+        </div>
+        <p class="text-justify text-lg">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse dictum lacinia
+          elementum. Praesent molestie vel sem rutrum accumsan. Curabitur fermentum nec erat eu
+          iaculis. Nullam urna mauris, varius in luctus et, faucibus id elit. Nam commodo elit nec
+          mauris rhoncus pellentesque.
+        </p>
+      </div>
+      <br />
+      <div>
+        <div class="relative">
+          <div
+            class="absolute top-1/2 -left-12 size-4 -translate-y-1/2 rounded-full bg-orange-600"
+          ></div>
+          <h1 class="mb-1.5 text-4xl font-bold text-orange-600">Misi</h1>
+        </div>
+        <ul class="list-disc text-lg">
+          <li>Lorem ipsum dolor sit amet</li>
+          <li>Consectetur adipiscing elit</li>
+          <li>Suspendisse dictum lacinia elementum</li>
+          <li>Praesent molestie vel sem rutrum accumsan</li>
+        </ul>
+      </div>
+    </div>
+  </section>
+</main>
--- src/routes/Navbar.svelte
+++ src/routes/Navbar.svelte
@@ -1,135 +1,135 @@
 <script lang="ts">
-	import logo1 from '$lib/assets/logo1.png';
-	import { onMount } from 'svelte';
+  import logo1 from '$lib/assets/logo1.png';
+  import { onMount } from 'svelte';
 
-	const links: { text: string; href: string }[] = [
-		{ text: 'Tentang', href: '#' },
-		{ text: 'Pembina', href: '#pembina' },
-		{ text: 'Discord', href: '#discord' },
-		{ text: 'Artikel', href: '#artikel' },
-		{ text: 'FAQ', href: '#faq' },
-		{ text: 'Kontak', href: '#kontak' }
-	];
+  const links: { text: string; href: string }[] = [
+    { text: 'Tentang', href: '#' },
+    { text: 'Pembina', href: '#pembina' },
+    { text: 'Discord', href: '#discord' },
+    { text: 'Artikel', href: '#artikel' },
+    { text: 'FAQ', href: '#faq' },
+    { text: 'Kontak', href: '#kontak' }
+  ];
 
-	let isDrawerOpen = $state(false);
-	let drawerHeight = $state(1000);
+  let isDrawerOpen = $state(false);
+  let drawerHeight = $state(1000);
 
-	onMount(() => {
-		drawerHeight = document.getElementById('drawer')?.clientHeight || 1000;
-	});
+  onMount(() => {
+    drawerHeight = document.getElementById('drawer')?.clientHeight || 1000;
+  });
 </script>
 
 <nav class="fixed top-0 left-0 z-10 flex h-16 w-screen bg-white text-slate-600 shadow-sm">
-	<div
-		class="mx-auto flex h-full w-full max-w-[125rem] items-center justify-between bg-white px-4 sm:px-6 md:px-12 lg:px-24 xl:px-36"
-	>
-		<section>
-			<a href="/" class="text-xl font-semibold text-orange-600 md:text-[1.4rem]">
-				<img src={logo1} alt="Logo" class="mr-0.5 inline size-9 md:mr-1.5 md:size-10" />
-				CryptoSharia
-			</a>
-		</section>
-		<section>
-			<!-- Mobile Hamburger Button -->
-			<button
-				id="hamburger-button"
-				class="mr-0.5 flex h-6 w-6 flex-col justify-center hover:cursor-pointer md:mr-0 md:hidden"
-				class:hamburger-active={isDrawerOpen}
-				aria-label="Toggle menu"
-				onclick={() => (isDrawerOpen = !isDrawerOpen)}
-			>
-				<span
-					class="h-[0.15rem] w-full rounded-full bg-orange-600 transition-opacity duration-300 ease-in-out"
-				></span>
-				<div class="relative mt-1.5 mb-2">
-					<span
-						class="absolute left-0 h-[0.15rem] w-full rounded-full bg-orange-600 transition-transform duration-300 ease-in-out"
-					></span>
-					<span
-						class="absolute left-0 h-[0.15rem] w-full rounded-full bg-orange-600 transition-transform duration-300 ease-in-out"
-					></span>
-				</div>
-				<span
-					class="h-[0.15rem] w-full rounded-full bg-orange-600 transition-opacity duration-300 ease-in-out"
-				></span>
-			</button>
-			<!-- /Mobile Hamburger Button -->
-			<!-- Mobile Menu (Drawer) -->
-			<div
-				id="drawer"
-				class="absolute left-0 -z-10 w-screen rounded-b-4xl border-b border-slate-200 bg-white px-6 shadow-md transition-transform duration-300 ease-in-out"
-				style={`top: -${drawerHeight}px`}
-				class:drawer-open={isDrawerOpen}
-			>
-				<ul class="flex flex-col">
-					{#each links as link}
-						{#if link.text !== 'Kontak'}
-							<li class="border-b border-slate-200 py-3">
-								<a
-									href={link.href}
-									onclick={() => setTimeout(() => (isDrawerOpen = false), 200)}
-									class="block w-full text-center font-medium transition-colors duration-300 ease-in-out hover:text-orange-600"
-									>{link.text}</a
-								>
-							</li>
-						{:else}
-							<li class="er-slate-200 py-3">
-								<a
-									href={link.href}
-									onclick={() => setTimeout(() => (isDrawerOpen = false), 200)}
-									class="block w-full rounded-full border-2 border-orange-600 bg-orange-600 px-4 py-1.5 text-center font-bold text-white transition-colors duration-300 ease-in-out hover:bg-white hover:text-orange-600"
-									>{link.text}</a
-								>
-							</li>
-						{/if}
-					{/each}
-				</ul>
-			</div>
-			<!-- /Mobile Menu (Drawer) -->
-			<!-- Desktop Menu -->
-			<ul class="hidden space-x-[1.5rem] md:flex lg:space-x-7">
-				{#each links as link}
-					{#if link.text !== 'Kontak'}
-						<li>
-							<a
-								href={link.href}
-								class="font-medium transition-colors duration-300 ease-in-out hover:text-orange-600"
-								>{link.text}</a
-							>
-						</li>
-					{:else}
-						<li>
-							<a
-								href={link.href}
-								class="rounded-full border-2 border-orange-600 bg-orange-600 px-4 py-1.5 font-bold text-white transition-colors duration-300 ease-in-out hover:bg-white hover:text-orange-600"
-								>{link.text}</a
-							>
-						</li>
-					{/if}
-				{/each}
-			</ul>
-			<!-- /Desktop Menu -->
-		</section>
-	</div>
+  <div
+    class="mx-auto flex h-full w-full max-w-[100rem] items-center justify-between bg-white px-4 sm:px-6 md:px-12 lg:px-24 xl:px-36"
+  >
+    <section>
+      <a href="/" class="text-xl font-semibold text-orange-600 md:text-[1.4rem]">
+        <img src={logo1} alt="Logo" class="mr-0.5 inline size-9 md:mr-1.5 md:size-10" />
+        CryptoSharia
+      </a>
+    </section>
+    <section>
+      <!-- Mobile Hamburger Button -->
+      <button
+        id="hamburger-button"
+        class="mr-0.5 flex h-6 w-6 flex-col justify-center hover:cursor-pointer md:mr-0 md:hidden"
+        class:hamburger-active={isDrawerOpen}
+        aria-label="Toggle menu"
+        onclick={() => (isDrawerOpen = !isDrawerOpen)}
+      >
+        <span
+          class="h-[0.15rem] w-full rounded-full bg-orange-600 transition-opacity duration-300 ease-in-out"
+        ></span>
+        <div class="relative mt-1.5 mb-2">
+          <span
+            class="absolute left-0 h-[0.15rem] w-full rounded-full bg-orange-600 transition-transform duration-300 ease-in-out"
+          ></span>
+          <span
+            class="absolute left-0 h-[0.15rem] w-full rounded-full bg-orange-600 transition-transform duration-300 ease-in-out"
+          ></span>
+        </div>
+        <span
+          class="h-[0.15rem] w-full rounded-full bg-orange-600 transition-opacity duration-300 ease-in-out"
+        ></span>
+      </button>
+      <!-- /Mobile Hamburger Button -->
+      <!-- Mobile Menu (Drawer) -->
+      <div
+        id="drawer"
+        class="absolute left-1/2 -z-10 w-screen -translate-x-1/2 transform rounded-b-4xl border-b border-slate-200 bg-white px-6 shadow-md transition-transform duration-300 ease-in-out"
+        style={`top: -${drawerHeight}px`}
+        class:drawer-open={isDrawerOpen}
+      >
+        <ul class="flex flex-col">
+          {#each links as link}
+            {#if link.text !== 'Kontak'}
+              <li class="border-b border-slate-200 py-3">
+                <a
+                  href={link.href}
+                  onclick={() => setTimeout(() => (isDrawerOpen = false), 200)}
+                  class="block w-full text-center font-medium transition-colors duration-300 ease-in-out hover:text-orange-600"
+                  >{link.text}</a
+                >
+              </li>
+            {:else}
+              <li class="er-slate-200 py-3">
+                <a
+                  href={link.href}
+                  onclick={() => setTimeout(() => (isDrawerOpen = false), 200)}
+                  class="block w-full rounded-full border-2 border-orange-600 bg-orange-600 px-4 py-1.5 text-center font-bold text-white transition-colors duration-300 ease-in-out hover:bg-white hover:text-orange-600"
+                  >{link.text}</a
+                >
+              </li>
+            {/if}
+          {/each}
+        </ul>
+      </div>
+      <!-- /Mobile Menu (Drawer) -->
+      <!-- Desktop Menu -->
+      <ul class="hidden space-x-[1.5rem] md:flex lg:space-x-7">
+        {#each links as link}
+          {#if link.text !== 'Kontak'}
+            <li>
+              <a
+                href={link.href}
+                class="font-medium transition-colors duration-300 ease-in-out hover:text-orange-600"
+                >{link.text}</a
+              >
+            </li>
+          {:else}
+            <li>
+              <a
+                href={link.href}
+                class="rounded-full border-2 border-orange-600 bg-orange-600 px-4 py-1.5 font-bold text-white transition-colors duration-300 ease-in-out hover:bg-white hover:text-orange-600"
+                >{link.text}</a
+              >
+            </li>
+          {/if}
+        {/each}
+      </ul>
+      <!-- /Desktop Menu -->
+    </section>
+  </div>
 </nav>
 
 <style>
-	:global(.hamburger-active > div > span:first-child) {
-		display: absolute;
-		transform: rotate(45deg);
-	}
+  :global(.hamburger-active > div > span:first-child) {
+    display: absolute;
+    transform: rotate(45deg);
+  }
 
-	:global(.hamburger-active > div > span:last-child) {
-		display: absolute;
-		transform: rotate(-45deg);
-	}
+  :global(.hamburger-active > div > span:last-child) {
+    display: absolute;
+    transform: rotate(-45deg);
+  }
 
-	:global(.hamburger-active > span:first-child),
-	:global(.hamburger-active > span:last-child) {
-		opacity: 0;
-	}
+  :global(.hamburger-active > span:first-child),
+  :global(.hamburger-active > span:last-child) {
+    opacity: 0;
+  }
 
-	:global(.drawer-open) {
-		transform: translateY(calc(100% + 4rem));
-	}
+  :global(.drawer-open) {
+    transform: translateY(calc(100% + 3.9rem));
+  }
 </style>
