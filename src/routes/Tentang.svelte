dfdeae3a3b9d65d37c78d1cf888b13a288172c58
Daffa Ilhami <mdaffailhami@gmail.com>
2025-09-06 10:29:03 +0800

    Make "Tentang" to be responsive

--- src/routes/Tentang.svelte
+++ src/routes/Tentang.svelte
@@ -5,8 +5,16 @@
   import sui from '$lib/assets/crypto-icons/sui.png';
 </script>
 
+{#snippet line(device: 'desktop' | 'mobile')}
+  {#if device === 'desktop'}
+    <div class="mx-8 hidden w-[1px] bg-orange-300 md:block lg:mx-12"></div>
+  {:else}
+    <div class="mr-8 ml-6 w-[1px] bg-orange-300 md:hidden lg:mx-12"></div>
+  {/if}
+{/snippet}
+
 <section
-  class="flex max-w-[100rem] flex-row items-stretch justify-between px-4 sm:px-6 md:px-12 lg:px-24 xl:px-36"
+  class="flex max-w-[100rem] flex-col items-stretch justify-between px-4 sm:px-6 md:flex-row md:px-12 lg:px-24 xl:px-36"
 >
   <div class="flex flex-1 flex-col rounded-4xl border border-slate-200 p-8 shadow-md">
     <h1 class="mb-1.5 text-3xl font-bold text-orange-600">Tentang <u><i>CryptoSharia</i></u></h1>
@@ -26,36 +34,38 @@
       <img src={sui} alt="Sui" class="size-8.5" />
     </div>
   </div>
-  <div class=" mx-12 w-[1px] bg-orange-300"></div>
-  <div class="flex flex-1 flex-col p-8 pl-0">
+  {@render line('desktop')}
+  <div class="flex flex-1 flex-row items-stretch p-8 pl-0 md:flex-col">
+    {@render line('mobile')}
     <div>
-      <div class="relative">
-        <div
-          class="absolute top-1/2 -left-14 size-4 -translate-y-1/2 rounded-full bg-orange-600"
-        ></div>
-        <h1 class="mb-1.5 text-3xl font-bold text-orange-600">Visi</h1>
+      <div>
+        <div class="relative">
+          <div
+            class="absolute top-1/2 -left-10 size-4 -translate-y-1/2 rounded-full bg-orange-600 lg:-left-14"
+          ></div>
+          <h1 class="mb-1.5 text-3xl font-bold text-orange-600">Visi</h1>
+        </div>
+        <p class="text-justify text-base">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse dictum lacinia
+          elementum. Praesent molestie vel sleft-10 left-10em rutrum accumsan. Curabitur fermentum
+          nec erat eu iaculis. Nullam urna mauris, varius in luctus et, faucibus id elit.
+        </p>
       </div>
-      <p class="text-justify text-base">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse dictum lacinia
-        elementum. Praesent molestie vel sem rutrum accumsan. Curabitur fermentum nec erat eu
-        iaculis. Nullam urna mauris, varius in luctus et, faucibus id elit. Nam commodo elit nec
-        mauris rhoncus pellentesque.
-      </p>
-    </div>
-    <br />
-    <div>
-      <div class="relative">
-        <div
-          class="absolute top-1/2 -left-14 size-4 -translate-y-1/2 rounded-full bg-orange-600"
-        ></div>
-        <h1 class="mb-1.5 text-3xl font-bold text-orange-600">Misi</h1>
+      <br />
+      <div>
+        <div class="relative">
+          <div
+            class="absolute top-1/2 -left-10 size-4 -translate-y-1/2 rounded-full bg-orange-600 lg:-left-14"
+          ></div>
+          <h1 class="mb-1.5 text-3xl font-bold text-orange-600">Misi</h1>
+        </div>
+        <ul class="list-disc text-base">
+          <li>Lorem ipsum dolor sit amet</li>
+          <li>Consectetur adipiscing elit</li>
+          <li>Suspendisse dictum lacinia elementum</li>
+          <li>Praesent molestie vel sem rutrum accumsan</li>
+        </ul>
       </div>
-      <ul class="list-disc text-base">
-        <li>Lorem ipsum dolor sit amet</li>
-        <li>Consectetur adipiscing elit</li>
-        <li>Suspendisse dictum lacinia elementum</li>
-        <li>Praesent molestie vel sem rutrum accumsan</li>
-      </ul>
     </div>
   </div>
 </section>
