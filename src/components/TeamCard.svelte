b96f49ece0a82933af234cb9a85a3fcefa02c2fe
Daffa Ilhami <mdaffailhami@gmail.com>
2025-10-11 12:56:22 +0800

    Update team

--- src/components/TeamCard.svelte
+++ src/components/TeamCard.svelte
@@ -1,15 +1,15 @@
 <script lang="ts">
-  let { name, bio, imageUrl }: { name: string; bio: string; imageUrl: string } = $props();
+  let { name, bio, image }: { name: string; bio: string; image: string } = $props();
 </script>
 
-<div class="w-60">
+<div class="w-62">
   <img
     data-aos="zoom-out"
     data-aos-duration="1000"
-    src={imageUrl}
+    src={image}
     alt={name}
-    class="mb-2 h-60 w-full rounded-2xl object-cover"
+    class="mb-2 h-62 w-full rounded-2xl object-cover"
   />
-  <h2 class="text-center text-2xl font-semibold text-orange-600">{name}</h2>
+  <h2 class="text-center text-xl font-semibold text-orange-600">{name}</h2>
   <p class="text-center text-slate-700">{bio}</p>
 </div>
--- src/routes/Team.svelte
+++ src/routes/Team.svelte
@@ -2,36 +2,38 @@
   import Divider from '../components/Divider.svelte';
   import TeamCard from '../components/TeamCard.svelte';
   import Title from '../components/Title.svelte';
+  import pembina1 from '$lib/assets/team/pembina1.png';
+  import pembina2 from '$lib/assets/team/pembina2.png';
+  import pembina3 from '$lib/assets/team/pembina3.png';
+  import pembina4 from '$lib/assets/team/pembina4.png';
+  import pembina5 from '$lib/assets/team/pembina5.png';
 
   const people = [
     {
-      name: 'Daffa Ilhami',
-      bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-      imageUrl:
-        'https://api-mainnet.suifrens.sui.io/suifrens/0x04a19ee8a526bd5f2fabcb903fd37e798a2a3b814ead7240a40cf405e4e831e6/svg'
+      name: 'K.H. Izzuddin Edi Siswanto, Lc., M.A., Ph.D.',
+      bio: 'Anggota DSN-MUI & DPS Dompet Dhuafa',
+      image: pembina1
     },
     {
-      name: 'Satoshi Nakamoto',
-      bio: 'Lorem ipsum dolor sit amet, sed do eiusmod tempor incididunt ut labore',
-      imageUrl: 'https://pbs.twimg.com/media/GxaYTrJXkAAuoZt.png'
+      name: 'Ustadz Ali Hasan Bawazier',
+      bio: 'Ketua DPP Perhimpunan Al Irsyad',
+      image: pembina2
     },
     {
-      name: 'Vitalik Buterin',
-      bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-      imageUrl: 'https://static.news.bitcoin.com/wp-content/uploads/2025/02/vbuterin1.png'
+      name: 'Ustadz Devin Halim Wijaya, B.B.A., M. Sc.',
+      bio: 'Alumni Idary LIPIA & IIUM, Malaysia',
+      image: pembina3
     },
     {
-      name: 'Anatoly Yakovenko',
-      bio: 'Lorem ipsum dolor sit amet, sed do eiusmod tempor incididunt ut labore',
-      imageUrl:
-        'https://external-preview.redd.it/solana-co-founder-anatoly-yakovenko-advocates-for-fair-v0-WXPCkWvUiBMFZdTUUbe2KIRtIUHTst3CX3kWq8IuO1k.jpg?width=1080&crop=smart&auto=webp&s=56e9b364c740f3b7ebeaebc0878f31ed83cb3e8d'
+      name: 'Ustadz Ade Setiawan, Lc.',
+      bio: 'Alumni Universitas Islam Madinah',
+      image: pembina4
     },
     {
-      name: 'Evan Cheng',
-      bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-      imageUrl:
-        'https://consensus-hongkong2025.coindesk.com/site/consensushongkong202/images/userfiles/speakers/a6264ed03f7dade12c995a8ad375a7fe.jpg'
-    }
+      name: 'Dea Saka Kurnia Putra, S.Tr.MP',
+      bio: 'Founder AKSI, Blockchain Society',
+      image: pembina5
+    },
   ];
 </script>
 
@@ -42,7 +44,7 @@
     class="flex w-full flex-row flex-wrap items-start justify-center gap-x-8 gap-y-8 px-6 pt-5 md:gap-x-24 md:gap-y-12 lg:gap-x-30 xl:gap-x-40"
   >
     {#each people as person}
-      <TeamCard name={person.name} bio={person.bio} imageUrl={person.imageUrl} />
+      <TeamCard name={person.name} bio={person.bio} image={person.image} />
     {/each}
   </div>
 </section>
