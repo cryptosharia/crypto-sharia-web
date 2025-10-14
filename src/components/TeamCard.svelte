d7b9aa8242d3976b9fd02dd8d91ca2c21a7937f6
Daffa Ilhami <mdaffailhami@gmail.com>
2025-10-14 19:24:08 +0800

    Add hover effect to team

--- src/components/TeamCard.svelte
+++ src/components/TeamCard.svelte
@@ -3,13 +3,13 @@
 </script>
 
 <div class="w-62">
-  <img
-    data-aos="zoom-out"
-    data-aos-duration="1000"
-    src={image}
-    alt={name}
-    class="mb-2 h-62 w-full rounded-2xl object-cover"
-  />
+  <div data-aos="zoom-out" data-aos-duration="1000">
+    <img
+      src={image}
+      alt={name}
+      class="mb-2 h-62 w-full rounded-2xl object-cover transition-transform duration-300 hover:scale-107"
+    />
+  </div>
   <h2 class="text-center text-xl font-semibold text-orange-600">{name}</h2>
   <p class="text-center text-slate-700">{bio}</p>
 </div>
